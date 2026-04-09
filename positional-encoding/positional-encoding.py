d7744345cefa899a67eb17e4491b7a145debc774
Tajinder <tajinderpalsingh61@gmail.com>
2026-04-09 07:30:02 +0530

    Update solution for positional-encoding

--- positional-encoding/positional-encoding.py
+++ positional-encoding/positional-encoding.py
@@ -7,9 +7,10 @@ def positional_encoding(seq_len, d_model, base=10000.0):
     """
 
     a = np.tile(np.arange(d_model), (seq_len, 1))
+    a = a//2
 
     pos = np.tile(np.arange(seq_len).reshape(seq_len, 1), (1, d_model))
-    b = pos / (base ** (2*(a//2)/d_model))
+    b = pos / (base ** (2*a/d_model))
     # b, b[:, 0::2], b[:, 1::2]
     b[:, 0::2] = np.sin(b[:, 0::2])
     b[:, 1::2] = np.cos(b[:, 1::2])
